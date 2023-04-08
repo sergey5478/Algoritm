f62b80825a06fcfc5dd5ae4c5d1c12d28b85c073
sergey <sergey.fomin.90@inbox.ru>
2023-04-08 15:37:23 +0400

    add_exception

---
+++ exception/seminar2/Program1.java
@@ -0,0 +1,11 @@
+package seminar2;
+
+/**
+ * Program1
+ */
+public class Program1 {
+
+    public static void main(String[] args) {
+        
+    }
+}
--- exception/seminarEX/Homework1.java
+++ exception/seminarEX/Homework1.java
@@ -1,5 +1,7 @@
 package seminarEX;
 
 public class Homework1 {
-    
+    public static void main(String[] args) {
+        
+    }
 }
